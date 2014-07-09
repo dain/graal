7db2a59b065f206dc49d7be83e5418ae8e3b576e
Doug Simon <doug.simon@oracle.com>
2014-07-09 19:02:29 +0200

    fixed deadlock in GraalVM under -Xcomp (JBS:GRAAL-48)

---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/NarrowNode.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes.calc;
+
+import com.oracle.graal.api.meta.*;
+import com.oracle.graal.compiler.common.type.*;
+import com.oracle.graal.graph.spi.*;
+import com.oracle.graal.lir.gen.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.nodes.type.*;
+
+/**
+ * The {@code NarrowNode} converts an integer to a narrower integer.
+ */
+public class NarrowNode extends IntegerConvertNode {
+
+    public NarrowNode(ValueNode input, int resultBits) {
+        super(StampTool.narrowingConversion(input.stamp(), resultBits), input, resultBits);
+    }
+
+    public static long narrow(long value, int resultBits) {
+        long ret = value & IntegerStamp.defaultMask(resultBits);
+        return SignExtendNode.signExtend(ret, resultBits);
+    }
+
+    @Override
+    public Constant convert(Constant c) {
+        return Constant.forPrimitiveInt(getResultBits(), narrow(c.asLong(), getResultBits()));
+    }
+
+    @Override
+    public Constant reverse(Constant input) {
+        long result = SignExtendNode.signExtend(input.asLong(), getResultBits());
+        return Constant.forPrimitiveInt(getInputBits(), result);
+    }
+
+    @Override
+    public boolean isLossless() {
+        return false;
+    }
+
+    @Override
+    public ValueNode canonical(CanonicalizerTool tool, ValueNode forValue) {
+        ValueNode ret = canonicalConvert(forValue);
+        if (ret != this) {
+            return ret;
+        }
+
+        if (forValue instanceof NarrowNode) {
+            // zzzzzzzz yyyyxxxx -(narrow)-> yyyyxxxx -(narrow)-> xxxx
+            // ==> zzzzzzzz yyyyxxxx -(narrow)-> xxxx
+            NarrowNode other = (NarrowNode) forValue;
+            return new NarrowNode(other.getValue(), getResultBits());
+        } else if (forValue instanceof IntegerConvertNode) {
+            // SignExtendNode or ZeroExtendNode
+            IntegerConvertNode other = (IntegerConvertNode) forValue;
+            if (getResultBits() == other.getInputBits()) {
+                // xxxx -(extend)-> yyyy xxxx -(narrow)-> xxxx
+                // ==> no-op
+                return other.getValue();
+            } else if (getResultBits() < other.getInputBits()) {
+                // yyyyxxxx -(extend)-> zzzzzzzz yyyyxxxx -(narrow)-> xxxx
+                // ==> yyyyxxxx -(narrow)-> xxxx
+                return new NarrowNode(other.getValue(), getResultBits());
+            } else {
+                if (other instanceof SignExtendNode) {
+                    // sxxx -(sign-extend)-> ssssssss sssssxxx -(narrow)-> sssssxxx
+                    // ==> sxxx -(sign-extend)-> sssssxxx
+                    return new SignExtendNode(other.getValue(), getResultBits());
+                } else if (other instanceof ZeroExtendNode) {
+                    // xxxx -(zero-extend)-> 00000000 00000xxx -(narrow)-> 0000xxxx
+                    // ==> xxxx -(zero-extend)-> 0000xxxx
+                    return new ZeroExtendNode(other.getValue(), getResultBits());
+                }
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public boolean inferStamp() {
+        return updateStamp(StampTool.narrowingConversion(getValue().stamp(), getResultBits()));
+    }
+
+    @Override
+    public void generate(NodeMappableLIRBuilder builder, ArithmeticLIRGenerator gen) {
+        builder.setResult(this, gen.emitNarrow(builder.operand(getValue()), getResultBits()));
+    }
+}
