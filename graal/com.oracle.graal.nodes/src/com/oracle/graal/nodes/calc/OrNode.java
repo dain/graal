12e50a3237daa2d3f743b279e23ea25499877d6c
Christian Wimmer <christian.wimmer@oracle.com>
2014-07-15 16:34:53 -0700

    Change API for stack walking to a visitor: TruffleRuntime#iterateFrames replaces TruffleRuntime#getStackTrace

---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/OrNode.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2011, 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes.calc;
+
+import com.oracle.graal.api.meta.*;
+import com.oracle.graal.compiler.common.type.*;
+import com.oracle.graal.graph.*;
+import com.oracle.graal.graph.spi.*;
+import com.oracle.graal.lir.gen.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.nodes.type.*;
+import com.oracle.graal.nodes.util.*;
+
+@NodeInfo(shortName = "|")
+public final class OrNode extends BitLogicNode {
+
+    public OrNode(ValueNode x, ValueNode y) {
+        super(StampTool.or(x.stamp(), y.stamp()), x, y);
+        assert x.stamp().isCompatible(y.stamp());
+    }
+
+    @Override
+    public boolean inferStamp() {
+        return updateStamp(StampTool.or(getX().stamp(), getY().stamp()));
+    }
+
+    @Override
+    public Constant evalConst(Constant... inputs) {
+        assert inputs.length == 2;
+        return Constant.forPrimitiveInt(PrimitiveStamp.getBits(stamp()), inputs[0].asLong() | inputs[1].asLong());
+    }
+
+    @Override
+    public ValueNode canonical(CanonicalizerTool tool, ValueNode forX, ValueNode forY) {
+        if (GraphUtil.unproxify(forX) == GraphUtil.unproxify(forY)) {
+            return forX;
+        }
+        if (forX.isConstant() && !forY.isConstant()) {
+            return new OrNode(forY, forX);
+        }
+        if (forX.isConstant()) {
+            return ConstantNode.forPrimitive(stamp(), evalConst(forX.asConstant(), forY.asConstant()));
+        } else if (forY.isConstant()) {
+            long rawY = forY.asConstant().asLong();
+            long mask = IntegerStamp.defaultMask(PrimitiveStamp.getBits(stamp()));
+            if ((rawY & mask) == mask) {
+                return ConstantNode.forIntegerStamp(stamp(), mask);
+            }
+            if ((rawY & mask) == 0) {
+                return forX;
+            }
+            return BinaryNode.reassociate(this, ValueNode.isConstantPredicate(), forX, forY);
+        }
+        return this;
+    }
+
+    @Override
+    public void generate(NodeMappableLIRBuilder builder, ArithmeticLIRGenerator gen) {
+        builder.setResult(this, gen.emitOr(builder.operand(getX()), builder.operand(getY())));
+    }
+}
---
+++ src/gpu/hsail/vm/hsailKernelArguments.hpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifndef GPU_HSAIL_VM_HSAIL_KERNEL_ARGUMENTS_HPP
+#define GPU_HSAIL_VM_HSAIL_KERNEL_ARGUMENTS_HPP
+
+#include "gpu_hsail.hpp"
+#include "runtime/signature.hpp"
+#include "hsailArgumentsBase.hpp"
+
+class HSAILKernelArguments : public HSAILArgumentsBase {
+  friend class Hsail;
+
+public:
+
+private:
+  // Kernel to push into
+  address _kernel;
+  void* _exceptionHolder;
+
+ public:
+    HSAILKernelArguments(address kernel, Symbol* signature, objArrayOop args, bool is_static, void* exceptionHolder) : HSAILArgumentsBase(signature, args, is_static) {
+        _kernel = kernel;
+        _exceptionHolder = exceptionHolder;
+        collectArgs();
+    }
+    virtual char* argsBuilderName() {return (char*)"HSAILKernelArguments";}
+    virtual void pushObject(void* obj) {
+        bool pushed = Hsail::_okra_push_object(_kernel, obj);
+        assert(pushed == true, "arg push failed");
+    }
+    virtual void pushBool(jboolean z) {
+        bool pushed = Hsail::_okra_push_boolean(_kernel, z);
+        assert(pushed == true, "arg push failed");
+    }
+    virtual void pushByte(jbyte b) {
+        bool pushed = Hsail::_okra_push_byte(_kernel, b);
+        assert(pushed == true, "arg push failed");
+    }
+
+    virtual void pushDouble(jdouble d) {
+        bool pushed = Hsail::_okra_push_double(_kernel, d);
+        assert(pushed == true, "arg push failed");
+    }
+
+    virtual void pushFloat(jfloat f) {
+        bool pushed = Hsail::_okra_push_float(_kernel, f);
+        assert(pushed == true, "arg push failed");
+    }
+
+    virtual void pushInt(jint i) {
+        bool pushed = Hsail::_okra_push_int(_kernel, i);
+        assert(pushed == true, "arg push failed");
+    }
+
+    virtual void pushLong(jlong j) {
+        bool pushed = Hsail::_okra_push_long(_kernel, j);
+        assert(pushed == true, "arg push failed");
+    }
+    virtual void pushTrailingArgs() {
+        if (UseHSAILDeoptimization) {
+            // Last argument is the exception info block
+            if (TraceGPUInteraction) {
+                tty->print_cr("[HSAIL] exception block=" PTR_FORMAT, _exceptionHolder);
+            }
+            pushObject(_exceptionHolder);
+        }
+    }
+
+    // For kernel arguments we don't pass the final int parameter
+    // since we use the HSAIL workitemid instruction in place of that int value
+    virtual void handleFinalIntParameter() {
+      ShouldNotReachHere();
+    }
+
+    // For kernel arguments, final obj parameter should be an object
+    // stream source array (already checked in the base class) so here we just pass it
+    virtual void handleFinalObjParameter(void* arg) {
+      ShouldNotReachHere();
+    }
+
+    virtual void collectArgs();
+
+};
+
+#endif  // GPU_HSAIL_VM_HSAIL_KERNEL_ARGUMENTS_HPP
